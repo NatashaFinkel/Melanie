3082f83189941bdfa4dc24cb39dd7e4fcf59da92
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-08 14:34:09 +0200

    Création du composant HeaderPageOption

---
+++ src/components/HeaderPageOption.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HeaderPageOption(pageUrl, pageTitle) {
+    <div className="menu-option">
+        <a href={pageUrl} className="nav-option">{pageTitle}</a>
+    </div>
+}
+
+export default HeaderPageOption;
