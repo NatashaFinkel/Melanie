abb6a3f3f2f4953d1056c31e1bd9523e00b7988e
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-08 15:00:48 +0200

    Correction du composant HeaderPageOption

--- src/components/HeaderPageOption.jsx
+++ src/components/HeaderPageOption.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function HeaderPageOption(pageUrl, pageTitle) {
-    <div className="menu-option">
-        <a href={pageUrl} className="nav-option">{pageTitle}</a>
-    </div>
+function HeaderPageOption({ pageUrl, pageTitle }) {
+    return (
+        <div className="menu-option">
+            <a href={pageUrl} className="nav-option">{pageTitle}</a>
+        </div>
+    );
 }
+
+HeaderPageOption.propTypes = {
+    pageUrl: PropTypes.string.isRequired,
+    pageTitle: PropTypes.string.isRequired,
+};
 
 export default HeaderPageOption;
