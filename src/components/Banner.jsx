36ea2ab5fed26748ee35111d215d53db6827d2a8
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-09 16:36:22 +0200

    Ajout d'une classe prop à la bannière

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,35 +4,39 @@ import { useLocation } from "react-router-dom";
 function Banner() {
     const location = useLocation();
     const pageUrl = location.pathname;
-    console.log(pageUrl);
     let bannerImgSrc;
+    let bannerClassName;
 
     switch (pageUrl) {
         case "/":
             bannerImgSrc = "/images/banner-sun-and-stars.jpg";
+            bannerClassName = "banner-img";
             break;
         case "/prestations":
             bannerImgSrc = "/images/banner-colza-field.jpg";
+            bannerClassName = "banner-img colza-field";
             break;
         case "/anniversaire":
             bannerImgSrc = "/images/star-banner.jpg";
+            bannerClassName = "banner-img";
             break;
         case "/disclaimer":
             bannerImgSrc = "/images/fleurs-blanches.jpeg";
+            bannerClassName = "banner-img white-flowers";
             break;
         case "/contact":
             bannerImgSrc = "/images/moon-boat.jpg";
+            bannerClassName = "banner-img moon-boat";
             break;
         default:
             bannerImgSrc = "/images/motivation.jpeg";
+            bannerClassName = "banner-img motivation";
             break;
     }
 
-    console.log(bannerImgSrc);
-
     return (
         <div>
-            <img src={bannerImgSrc} alt="bannière décorative" className="banner-img"></img>
+            <img src={bannerImgSrc} alt="bannière décorative" className={bannerClassName}></img>
         </div>
     )
 }
