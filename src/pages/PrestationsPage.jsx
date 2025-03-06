e429aca7704754154193046b15194e420b4c8a2d
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-03-06 23:15:34 +0100

    Feature : implémentation du titre + txt de PrestationsPage.jsx

--- src/pages/PrestationsPage.jsx
+++ src/pages/PrestationsPage.jsx
@@ -1,9 +1,19 @@
 function PrestationsPage() {
     return (
-        <div>
-            <p>Voici la page PRESTATIONS</p>
-        </div>
-    )
+        <main>
+            <section className="center-display">
+                <div className="center-display main-content">
+                    <h2>Tout mon savoir-faire, à votre service !</h2>
+                    <p>Toujours avec bienveillance, beaucoup d'écoute, dans le désir d'être utile, voire réconfortante... sans oublier dans certains cas un soupçon d'humour, car le rire, la légèreté sont autant de facteurs de dédramatisation, voire de guérison.</p>
+                </div>
+            </section>
+            <section className="center-display card-container">
+                 {/* TODO: mise à jour dépendances*/}
+                {/* TODO: fichier json pour les cards */}
+                 {/* TODO: composant card */}
+            </section>
+        </main>
+    );
 }
 
 export default PrestationsPage;
