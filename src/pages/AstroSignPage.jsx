502d6893d1ac15301a498e1f7d92229dd81364cb
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-03-21 19:06:03 +0100

    Refacto : clean code et indentation de AstroSignPage.jsx

--- src/pages/AstroSignPage.jsx
+++ src/pages/AstroSignPage.jsx
@@ -5,14 +5,28 @@ function AstroSignPage() {
                 <div className="center-display astroSignPage">
                     <h2>Le signe du mois</h2>
                     <div className="astroSignPage-main-content">
-                        <img src="./assets/astro-sign.jpeg" alt="Signes astrologiques" className="astroSign-img" />
+                        <img
+                            src="./assets/astro-sign.jpeg"
+                            alt="Signes astrologiques"
+                            className="astroSign-img"
+                        />
                         <div className="astroSignPage-txt-container">
                             <p>Bonjour, amis des GuidanSes de Mélanie !</p>
                             <p>Nous voici maintenant dans le mois des POISSONS.</p>
-                            <p>C'est votre anniversaire ce mois-ci ? Sachez que vous bénéficiez d'une réduction de 20 % sur les GuidanSes individuelles privées.</p>
-                            <p>Attention, c'est une offre à durée limitée : elle est valable uniquement si votre propre signe astrologique est identique à celui de la date de la prestation.</p><br></br>
-                            <p>JOYEUX ANNIVERSAIRE, MES AMIS/AMIES POISSONS !!
-                            🎂 👏 🥂 🥂🍀 🌺 🌺 👀 👍 💕</p>
+                            <p>
+                                C'est votre anniversaire ce mois-ci ? Sachez que vous bénéficiez
+                                d'une réduction de 20 % sur les GuidanSes individuelles privées.
+                            </p>
+                            <p>
+                                Attention, c'est une offre à durée limitée : elle est valable
+                                uniquement si votre propre signe astrologique est identique à
+                                celui de la date de la prestation.
+                            </p>
+                            <br></br>
+                            <p>
+                                JOYEUX ANNIVERSAIRE, MES AMIS/AMIES POISSONS !! 🎂 👏 🥂 🥂🍀 🌺
+                                🌺 👀 👍 💕
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -21,4 +35,4 @@ function AstroSignPage() {
     );
 }
 
-export default AstroSignPage;  
+export default AstroSignPage;
