90e062fcc550d8e871d8252ad51ce5174f4360d7
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-03-21 19:04:03 +0100

    Feature : implémentation de la page AstroSignPage

--- src/pages/AstroSignPage.jsx
+++ src/pages/AstroSignPage.jsx
@@ -1,8 +1,23 @@
 function AstroSignPage() {
     return (
-        <div>
-            <p>Voici la page du signe astrologique du mois</p>
-        </div>
+        <main>
+            <section className="center-display">
+                <div className="center-display astroSignPage">
+                    <h2>Le signe du mois</h2>
+                    <div className="astroSignPage-main-content">
+                        <img src="./assets/astro-sign.jpeg" alt="Signes astrologiques" className="astroSign-img" />
+                        <div className="astroSignPage-txt-container">
+                            <p>Bonjour, amis des GuidanSes de Mélanie !</p>
+                            <p>Nous voici maintenant dans le mois des POISSONS.</p>
+                            <p>C'est votre anniversaire ce mois-ci ? Sachez que vous bénéficiez d'une réduction de 20 % sur les GuidanSes individuelles privées.</p>
+                            <p>Attention, c'est une offre à durée limitée : elle est valable uniquement si votre propre signe astrologique est identique à celui de la date de la prestation.</p><br></br>
+                            <p>JOYEUX ANNIVERSAIRE, MES AMIS/AMIES POISSONS !!
+                            🎂 👏 🥂 🥂🍀 🌺 🌺 👀 👍 💕</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 }
 
