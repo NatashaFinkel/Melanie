3ffeaa782f549f415cae7ed7f18475ed096d72cd
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-05-01 18:19:02 +0200

    Feature : implémentation de la page contact

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,16 @@
 function ContactPage() {
     return (
-        <div>
-            <p>Voici la page CONTACT</p>
-        </div>
+        <main>
+            <section className="contact-page-main-section">
+                <div className="contact-details-div">
+                    <h2>Contactez-moi !</h2>
+                    <p>Mélanie Delaunay<br></br>06 63 66 54 20</p>
+                    <p>Melaniedelaunay@yahoo.com</p>
+                    <p>N'hésitez pas à laisser un message sur le répondeur si je suis en consultation !<br></br><br></br>
+                        Je vous recontacterai au plus vite.</p>
+                </div>
+            </section>
+        </main>
     );
 }
 
