0bb24be936718933590f34a4b24c543435edd385
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-05-02 16:57:16 +0200

    Feature : implémentation du footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import AppRouter from "./components/AppRouter";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <AppRouter />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer id="footer">
+            <a href="#footer">Condition générales d'utilisation</a>
+            <a href="#footer">Condition générales de vente</a>
+        </footer>
+    );
+}
+
+export default Footer;
