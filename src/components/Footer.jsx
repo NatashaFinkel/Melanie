619b96acfd9e94c0d59e9f1467617c8de0053e05
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-05-04 18:14:00 +0200

    Feature : implémentation accès aux réseaux sociaux dans le footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 function Footer() {
     return (
         <footer id="footer">
-            <a href="#footer">Condition générales d'utilisation</a>
-            <a href="#footer">Condition générales de vente</a>
+            <a href="#footer">CGU, CGV</a>
+            <a href="https://www.youtube.com/@lesguidanSesdemelanie" target="_blank" rel="noopener noreferrer">
+                <img src="/assets/youtube-logo.svg" alt="Logo YouTube"></img>
+            </a>
+
+            <a href="https://www.facebook.com/GuidanSesMelanie" target="_blank" rel="noopener noreferrer">
+                <img src="/assets/facebook-logo.svg" alt="Logo Facebook"></img>
+            </a>
         </footer>
     );
 }
