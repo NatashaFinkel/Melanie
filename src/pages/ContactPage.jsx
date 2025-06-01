48c24bd1059266b34167713406871147033283bf
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-06-01 10:15:42 +0200

    Refacto : refactorisation de ContactPage.jsx grâce à GenerateParagrahTxtContent.jsx

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,13 +1,13 @@
+import GenerateParagrahTxtContent from "../components/GenerateParagrahTxtContent";
+import contactPageTxt from "../json/contactPage-txt.json"
+
 function ContactPage() {
     return (
         <main>
             <section className="contact-page-main-section">
                 <div className="contact-details-div">
                     <h2>Contactez-moi !</h2>
-                    <p>Mélanie Delaunay<br></br>06 63 66 54 20</p>
-                    <p>Melaniedelaunay@yahoo.com</p>
-                    <p>N'hésitez pas à laisser un message sur le répondeur si je suis en consultation !<br></br><br></br>
-                        Je vous recontacterai au plus vite.</p>
+                    <GenerateParagrahTxtContent page="contactPage" jsonFile={contactPageTxt} textType="p" textProp="paragraph" />
                 </div>
             </section>
         </main>
