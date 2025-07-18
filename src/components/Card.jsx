2ed56c499e01e509a05bd9d13c4948de7cf3224f
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-07-18 17:40:26 +0200

    Refacto : indentation du code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,35 +2,35 @@ import { useState } from "react";
 import Modal from "./Modal";
 
 const Card = ({ cards }) => {
-    const [selectedCard, setSelectedCard] = useState(null);
+  const [selectedCard, setSelectedCard] = useState(null);
 
-    const handleCardClick = (card) => {
-        setSelectedCard(card);
-    };
+  const handleCardClick = (card) => {
+    setSelectedCard(card);
+  };
 
-    const handleCloseModal = () => {
-        setSelectedCard(null);
-    };
+  const handleCloseModal = () => {
+    setSelectedCard(null);
+  };
 
-    return (
-        <div className="card-container">
-            {cards.map((card) => (
-                <div
-                    key={card.id}
-                    className="card"
-                    onClick={() => handleCardClick(card)}
-                >
-                    <div>
-                        <img src={card.imgSrc} alt={card.imgAlt} />
-                    </div>
-                    <h3>{card.name}</h3>
-                    <p>{card.cardDescription}</p>
-                    <span className="badge">{card.badge}</span>
-                </div>
-            ))}
-            <Modal card={selectedCard} onClose={handleCloseModal} />
+  return (
+    <div className="card-container">
+      {cards.map((card) => (
+        <div
+          key={card.id}
+          className="card"
+          onClick={() => handleCardClick(card)}
+        >
+          <div>
+            <img src={card.imgSrc} alt={card.imgAlt} />
+          </div>
+          <h3>{card.name}</h3>
+          <p>{card.cardDescription}</p>
+          <span className="badge">{card.badge}</span>
         </div>
-    );
+      ))}
+      <Modal card={selectedCard} onClose={handleCloseModal} />
+    </div>
+  );
 };
 
 export default Card;
